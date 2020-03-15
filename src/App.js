17a3194dffa60272fd767da912893a3a466ad75e
Aleksandr Kondratskii <akondratsky@yandex.ru>
2020-03-16 00:03:12 +0400

    added new presenter script probably

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
+import yesImage from './yes.jpg';
+import noImage from './no.jpg';
 
-import formatDate from 'date-fns/lightFormat';
 import differenceInMinutes from 'date-fns/differenceInMinutes';
 
+const MAX_DIFFERENCE = 5; // if difference more than MAX_DIFFERENCE we thinking he is away
+
 const App = () => {
-  const [serverRunTime, setServerRunTime] = useState(0);
-  const [lastUpdateTime, setLastUpdateTime] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isWorking, setIsWorking] = useState(false);
 
   useEffect(() => {
     const updateTimings = async () => {
       const {
-        serverRunTime,
+        // serverRunTime,
         lastUpdateTime
       } = await fetch('https://arcane-fortress-59742.herokuapp.com/api/get')
         .then(res => res.json());
-      setServerRunTime(serverRunTime);
-      setLastUpdateTime(lastUpdateTime);
+      
+      if (!lastUpdateTime || differenceInMinutes(Date.now(), lastUpdateTime) > MAX_DIFFERENCE) {
+        setIsWorking(false);
+      } else {
+        setIsWorking(true)
+      }
+
+      if (!isLoaded) setIsLoaded(true);
       setTimeout(updateTimings, 5000);
     }
     updateTimings();
-  }, []);
+  }, [isLoaded]);
+
+  const wokingImageComponent = isWorking ?
+    <img src={yesImage} alt="he is working"/>
+    : <img src={noImage} alt="he is not working" />;
 
   return (
     <div className="app">
-      { serverRunTime && 
-        <p>
-          <span className="text-muted">Server was runned: </span>
-          {formatDate(serverRunTime, 'HH:mm dd.MM.yyyy')}
-        </p>
-      }
-      { lastUpdateTime &&
-        <p>
-          <span className="text-muted">Minutes from last update: </span>
-          {differenceInMinutes(Date.now(), lastUpdateTime)}
-        </p>
+      { isLoaded ?
+        wokingImageComponent
+        : <p>loading status...</p>
       }
     </div>
   );
